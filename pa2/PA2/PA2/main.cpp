06a25bf51edb34459b8db8586438125ef8818977
WhiteWolf299 <sylvanbrander98@gmail.com>
2018-09-17 13:32:28 -0700

    Finished creating the class and working on the input from files and from the user.

--- pa2/PA2/PA2/main.cpp
+++ pa2/PA2/PA2/main.cpp
@@ -32,6 +32,65 @@ public:
 	int get_line_number(void);
 	
 };
+void Customer::set_Customer_id(int id)
+{
+	Customer_id = id;
+}
+
+void Customer::set_arrival_time(int arrival)
+{
+	arrival_time = arrival;
+}
+
+void Customer::set_time_to_check_out(int checkout)
+{
+	time_to_check_out = checkout;
+}
+
+void Customer::set_departure_time(int depart)
+{
+	departure_time = depart;
+}
+
+void Customer::set_total_wait(int wait)
+{
+	total_wait = wait;
+}
+
+void Customer::set_line_number(int line)
+{
+	line_number = line;
+}
+
+int Customer::get_Customer_id(void)
+{
+	return Customer_id;
+}
+
+int Customer::get_arrival_time(void)
+{
+	return arrival_time;
+}
+
+int Customer::get_time_to_check_out(void)
+{
+	return time_to_check_out;
+}
+
+int Customer::get_departure_time(void)
+{
+	return departure_time;
+}
+
+int Customer::get_total_wait(void)
+{
+	return total_wait;
+}
+
+int Customer::get_line_number(void)
+{
+	return line_number;
+}
 
 
 
